d184a3524ebb1872a0942c748d41012b2b77a1a4
happywhilecoding <jon@expungeus>
2016-06-13 22:56:20 -0700

    Initial commit

---
+++ findSimilar.js
@@ -0,0 +1,58 @@
+/* Model Data */
+
+var doctors = {};
+
+doctors.Scott = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Kiira = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Matthew = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Benjamin = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Negar = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Julian = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Preston = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Lindsay = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
+doctors.Donald = {
+		specialty: 'chronic',
+		area: 'urology',
+		review: 9,
+		experience: 4
+};
